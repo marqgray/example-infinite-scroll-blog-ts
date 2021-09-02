1888053e69e64a5f8bf384a3e6d73adc745febfa
Marq Gray <marquis.s.sandifer@gmail.com>
2021-09-02 17:20:17 -0500

    Load initial posts.

--- script.ts
+++ script.ts
@@ -4,4 +4,35 @@ const app = new (class {
     loading: <HTMLDivElement>document.querySelector(".loader"),
     filter: <HTMLInputElement>document.getElementById("filter"),
   };
+  limit: number = 3;
+  page: number = 1;
+
+  constructor() {
+    this.showPosts();
+  }
+
+  async getPosts(): Promise<JsonPlaceholderPostList> {
+    const res = await fetch(
+      `http://jsonplaceholder.typicode.com/posts?_limit=${this.limit}&_page=${this.page}`
+    );
+
+    return await res.json();
+  }
+
+  async showPosts() {
+    const posts = await this.getPosts();
+
+    posts.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.classList.add("post");
+      postElement.innerHTML = `
+              <div class="number">${post.id}</div>
+              <div class="post-info">
+                <h2 class="post-title">${post.title}</h2>
+                <p class="post-body">${post.body}</p>
+              </div>
+            `;
+      this.htmlElements.postsContainer.appendChild(postElement);
+    });
+  }
 })();
